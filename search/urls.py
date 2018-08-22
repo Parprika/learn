e7675e1144b0229ce93928bd93b240cd177c4b04
QiuYanlin <32633581+Parprika@users.noreply.github.com>
2018-08-22 18:39:26 +0800

    Update urls.py

--- search/urls.py
+++ search/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, re_path
 from search import views
 
 urlpatterns = [
-
+	path('n/', views.search_note),
+	path('u/', views.search_user),
+	path('a/', views.search_activity),
 ]
