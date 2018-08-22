12f69caf59b79cb9d612448e64430904e2589eb3
QiuYanlin <32633581+Parprika@users.noreply.github.com>
2018-08-22 18:34:20 +0800

    添加收藏笔记,删除笔记功能

--- note/urls.py
+++ note/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
 	re_path('(?P<noteid>\d+)/$', views.note_detail),
 	re_path('(?P<noteid>\d+)/edit/', views.note_edit),
 	path('delete/', views.note_delete),
-	re_path('explore/', views.note_explore),
 	path('fond/', views.fond),
 	path('cancel_fond/', views.cancel_fond),
 ]
--- note/views.py
+++ note/views.py
@@ -40,8 +40,6 @@ def note_new(request):
 				try:
 					with transaction.atomic():
 						models.Notes.objects.create(**note_dict)
-						note_count = models.UserInfo.objects.get(userid=userid).notes_count
-						models.UserInfo.objects.filter(userid=userid).update(notes_count=note_count + 1)
 						ret['status'] = 'success'
 				except Exception as e:
 					ret['error'] = e.args
@@ -65,13 +63,11 @@ def note_detail(request, noteid):
 		note = models.Notes.objects.get(noteid=noteid)
 		if userid == note.note_user_id:
 			is_author = True
-		else:
-			read_count = models.Notes.objects.get(noteid=noteid).read_count
-			models.Notes.objects.filter(noteid=noteid).update(read_count=read_count + 1)
 		fond = models.UserFondNotes.objects.filter(noteid_id=noteid, userid_id=userid)
 		if fond:
 			is_fond = True
-		return render(request, 'note_detail.html', {'data': data, 'note': note, 'is_author': is_author, 'is_fond': is_fond})
+		return render(request, 'note_detail.html',
+					  {'data': data, 'note': note, 'is_author': is_author, 'is_fond': is_fond})
 
 
 def note_edit(request, noteid):
@@ -129,13 +125,11 @@ def note_delete(request):
 	if request.method == 'POST':
 		ret = {'status': 'fail', 'error': None}
 		noteid = request.POST.get('noteid')
-		note_obj = models.Notes.objects.get(noteid=noteid)
-		if request.session.get('userid') == note_obj.note_user_id:
+		note = models.Notes.objects.get(noteid=noteid)
+		if request.session.get('userid') == note.note_user_id:
 			try:
 				with transaction.atomic():
 					models.Notes.objects.filter(noteid=noteid).delete()
-					note_count = models.UserInfo.objects.get(userid=note_obj.note_user_id).notes_count
-					models.UserInfo.objects.filter(userid=note_obj.note_user_id).update(notes_count=note_count - 1)
 					ret['status'] = 'success'
 			except Exception as e:
 				ret['error'] = e.args
@@ -143,50 +137,6 @@ def note_delete(request):
 			ret['status'] = 'unauthorized'
 		return HttpResponse(json.dumps(ret))
 
-
-def note_explore(request):
-	"""
-	查找用户笔记
-	:param request:
-	:return:
-	"""
-	if request.method == 'GET':
-		keyword = request.GET.get('keyword')
-		tag = request.GET.get('tag_id', '0')
-		sort = request.GET.get('sort_by', '0')
-		sort_rule = {
-			'0': 'create_time',
-			'1': '-create_time',
-			'2': 'fond_count',
-			'3': '-fond_count',
-			'4': 'read_count',
-			'5': '-read_count'
-		}
-		sort_select = {
-			'0': '按时间升序',
-			'1': '按时间降序',
-			'2': '按收藏量升序',
-			'3': '按收藏量降序',
-			'4': '按阅读量升序',
-			'5': '按阅读量降序'
-		}
-		kwargs = {
-			'keyword': keyword,
-			'tag_id': tag,
-			'sort_by': sort
-		}
-		if keyword:
-			note_obj = models.Notes.objects.filter(
-				Q(title__contains=keyword) | Q(summary__contains=keyword) | Q(content__contains=keyword)).filter(
-				readlimit_id='1')
-		else:
-			note_obj = models.Notes.objects.filter(readlimit_id='1')
-		if tag != '0':
-			note_obj = note_obj.filter(notetag_id=tag)
-		note_obj = note_obj.order_by(sort_rule[sort])
-		tag_obj = models.NoteTag.objects.all()
-		return render(request, 'note_explore.html',
-					  {'note_obj': note_obj, 'tag_obj': tag_obj, 'sort_select': sort_select, 'kwargs': kwargs})
 
 @csrf_exempt
 def fond(request):
@@ -206,15 +156,11 @@ def fond(request):
 			else:
 				try:
 					with transaction.atomic():
-						fond_obj = models.UserFondNotes.objects.filter(noteid_id=noteid, userid_id=userid)
-						if fond_obj:
+						fond = models.UserFondNotes.objects.filter(noteid_id=noteid, userid_id=userid)
+						if fond:
 							ret['error'] = '已收藏笔记,重复操作.'
 						else:
 							models.UserFondNotes.objects.create(noteid_id=noteid, userid_id=userid)
-							note_fond_count = models.Notes.objects.get(noteid=noteid).fond_count
-							models.Notes.objects.filter(noteid=noteid).update(fond_count=note_fond_count + 1)
-							user_fond_count = models.UserInfo.objects.get(userid=userid).fond_notes_count
-							models.UserInfo.objects.filter(userid=userid).update(fond_notes_count=user_fond_count + 1)
 							ret['status'] = 'success'
 				except Exception as e:
 					ret['error'] = e.args
@@ -233,16 +179,11 @@ def cancel_fond(request):
 	if request.method == 'POST':
 		ret = {'status': 'fail', 'error': None}
 		noteid = request.POST.get('noteid')
-		note_user = request.POST.get('userid')
 		userid = request.session.get('userid')
 		if userid:
 			try:
 				with transaction.atomic():
 					models.UserFondNotes.objects.filter(noteid_id=noteid, userid_id=userid).delete()
-					note_fond_count = models.Notes.objects.get(noteid=noteid).fond_count
-					models.Notes.objects.filter(noteid=noteid).update(fond_count=note_fond_count - 1)
-					user_fond_count = models.UserInfo.objects.get(userid=userid).fond_notes_count
-					models.UserInfo.objects.filter(userid=userid).update(fond_notes_count=user_fond_count - 1)
 					ret['status'] = 'success'
 			except Exception as e:
 				ret['error'] = e.args
